063773132d61a7059c0aecdc103cf74a92300b99
keshav6606 <139983935+keshav6606@users.noreply.github.com>
2026-02-14 20:06:37 +0530

    feat: Switch shortener to arolinks.com and restore API key

--- info.py
+++ info.py
@@ -69,8 +69,8 @@ TUTORIAL = environ.get("TUTORIAL", "https://t.me/how_to_downlod_7")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/how_to_downlod_7")
 TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/how_to_downlod_7")
 
-SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "v2links.com")
-SHORTENER_API = environ.get("SHORTENER_API", "3f2562bc90d63cf9b355d30d852")
+SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "arolinks.com")
+SHORTENER_API = environ.get("SHORTENER_API", "7179d3dc854e96df2562bc90d63cf9b35d30d852")
 
 MOVIE_UPDATE_NOTIFICATION = bool(environ.get("MOVIE_UPDATE_NOTIFICATION", True))
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
