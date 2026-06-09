d71aca7c8a9d91c1a68383fefde51aabf0f0531a
keshavsingh77 <keshavrajlkr2@gmail.com>
2026-06-09 18:53:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,7 +69,7 @@ TUTORIAL = environ.get("TUTORIAL", "https://t.me/how_to_downlod_7")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/how_to_downlod_7")
 TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/how_to_downlod_7")
 
-SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "https://sarkaritrend.boats/api")
+SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "sarkaritrend.boats")
 SHORTENER_API = environ.get("SHORTENER_API", "93319f88d8487f0281f1c389aeec1ddd711186e4")
 
 MOVIE_UPDATE_NOTIFICATION = bool(environ.get("MOVIE_UPDATE_NOTIFICATION", True))
